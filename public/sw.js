5d626ff5bcf7bd3b12ae5e29a5aea89401de5d12
Liz Mitchell <lizvmitchell@gmail.com>
2020-02-03 10:41:11 -0500

    many lighthouse checks

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,21 @@
+const cacheName = 'cache-v1'
+const resourcesToPrecache = [
+  '/',
+  'index.html',
+  'style.css',
+  'Bullhorn192.png',
+  'Bullhorn512.png',
+  'favicon.ico',
+  'bundle.js'
+]
+
 self.addEventListener('install', event => {
-  console.log('Install event!')
+  console.log('Service worker install event!')
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(resourcesToPrecache)
+    })
+  )
 })
 
 self.addEventListener('activate', event => {
@@ -8,4 +24,9 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('Fetch intercepted for:', event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request)
+    })
+  )
 })
