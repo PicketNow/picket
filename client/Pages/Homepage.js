2b702137936702dae1a2d0166d34d8ceda82db7e
ChloeJ <chloe.jandsten@gmail.com>
2020-02-13 18:02:37 -0500

    FEATURE: Added buttons on homepage

--- client/Pages/Homepage.js
+++ client/Pages/Homepage.js
@@ -1,17 +1,50 @@
 import React from 'react'
 import AllInterests from '../components/interestButton.js'
 import FeaturedEvents from '../components/featuredEvent.js'
-import Jumbo from '../components/Jumbo.js'
 import MainFeaturedPost from '../components/jumbotest'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div>
-        <MainFeaturedPost />
-        <FeaturedEvents />
-        <AllInterests />
-      </div>
+      <Grid>
+        <Grid>
+          <MainFeaturedPost />
+        </Grid>
+        <Grid container direction="column">
+          <Grid container direction="row" justify="space-evenly">
+            <Grid item>
+              <Button
+                variant="outlined"
+                color="default"
+                size="large"
+                href="/events"
+              >
+                See All Events
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="outlined"
+                color="default"
+                size="large"
+                href="/search"
+              >
+                Filter Events
+              </Button>
+            </Grid>
+            <br /> <br /> <br /> <br />
+          </Grid>
+
+          <Grid>
+            <FeaturedEvents />
+          </Grid>
+          <Grid>
+            <AllInterests />
+          </Grid>
+        </Grid>
+      </Grid>
     )
   }
 }
